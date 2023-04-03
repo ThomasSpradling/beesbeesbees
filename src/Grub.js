121c9a2cc2d0c3607e51cc6a2a0d52b5c973fc8b
Thomas Spradling <thomasspradling13@gmail.com>
2023-04-03 11:32:47 -0700

    Finished Assignment

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,10 @@
-var Grub = function() {
+const Grub = function(age, color, food) {
+  this.age = age || 0;
+  this.color = color || 'pink';
+  this.food = food || 'jelly';
+};
+
+Grub.prototype.eat = function () {
+  console.log('It is eating');
 };
 
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,14 @@
-var HoneyMakerBee = function() {
+var HoneyMakerBee = function(age, color, food, job) {
+  Bee.call(this, age, color, food, job);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
-
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+HoneyMakerBee.prototype.makeHoney = function () {
+  this.honeyPot++;
+};
+HoneyMakerBee.prototype.giveHoney = function () {
+  this.honeyPot--;
+};
